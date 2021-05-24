97145512a5108bafd1377a9bfdfe100f8bfde0a5
wivarn <wivarn@gmail.com>
2021-05-24 02:05:02 -0700

    try red

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-wrap items-center bg-green-400 p-3 mt-3">
+    <footer className="flex flex-wrap items-center bg-red-900 p-3 mt-3">
       <div className="text-white">This is the footer</div>
     </footer>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -15,19 +15,19 @@ function LoggedInNav() {
   return (
     <div className="lg:flex-row lg:ml-auto lg:w-auto lg:items-center items-start flex flex-col lg:h-auto">
       <Link href="listings/new">
-        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-green-600 hover:text-white">
+        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
           <CurrencyDollarIcon className="h-8 w-8" />
           <span className="text-xs font-bold">Sell stuff</span>
         </a>
       </Link>
       <Link href="/">
-        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-green-600 hover:text-white">
+        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
           <ShoppingCartIcon className="h-8 w-8" />
           <span className="text-xs font-bold">Cart</span>
         </a>
       </Link>
       <Link href="/">
-        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-green-600 hover:text-white">
+        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
           <UserCircleIcon className="h-8 w-8" />
           <span className="text-xs font-bold">username goes here</span>
         </a>
@@ -40,7 +40,7 @@ function LoggedOutNav() {
   return (
     <div className="lg:flex-row lg:ml-auto lg:w-auto lg:items-center items-start flex flex-col lg:h-auto">
       <Link href="/login">
-        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-green-600 hover:text-white">
+        <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
           <UserCircleIcon className="h-8 w-8" />
           <span className="text-xs font-bold">Login</span>
         </a>
@@ -64,7 +64,7 @@ export default function Header() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header>
-        <nav className="flex flex-wrap items-center bg-green-400 p-3">
+        <nav className="flex flex-wrap items-center bg-red-900 p-3">
           <Link href="/">
             <a className="inline-flex p-2 mr-4 text-white">
               {/* Placeholder icon */}
@@ -75,7 +75,7 @@ export default function Header() {
             </a>
           </Link>
           <button
-            className="inline-flex p-3 m-1 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white"
+            className="inline-flex p-3 m-1 hover:bg-red-700 rounded lg:hidden text-white ml-auto hover:text-white"
             onClick={handleClick}
           >
             <MenuIcon className="w-6 h-6" />
