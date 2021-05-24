ae1b453dca46dafa4a924ecafb8c73accaad4a32
wivarn <wivarn@gmail.com>
2021-05-24 14:35:20 -0700

    update create listing form

--- components/forms/fields.tsx
+++ components/forms/fields.tsx
@@ -21,3 +21,19 @@ export const TextField: React.FC<TextFieldProps> = ({ label, ...props }) => {
     </div>
   );
 };
+
+export const NumberField: React.FC<TextFieldProps> = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props);
+  return (
+    <div className="my-2">
+      <label htmlFor={props.name}>{label}</label>
+      <input type="number" className="text-black" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error text-red-300 italic">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
