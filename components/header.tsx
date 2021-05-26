96e6a59c3c0bc004bde56930ec48712742921e38
Kevin Legere <kevin.legere@hey.com>
2021-05-26 14:18:47 -0700

    cleaned up the header

--- components/header.tsx
+++ components/header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
     return (
       <>
         <Link href="/login">
-          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
+          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-blue-600 hover:text-white">
             <UserCircleIcon className="h-8 w-8" />
             <span className="text-xs font-bold">Login</span>
           </a>
@@ -40,19 +40,19 @@ export default function Header() {
     return (
       <>
         <Link href="/listings/new">
-          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
+          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-blue-600 hover:text-white">
             <CurrencyDollarIcon className="h-8 w-8" />
             <span className="text-xs font-bold">Sell stuff</span>
           </a>
         </Link>
         <Link href="/">
-          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
+          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-blue-600 hover:text-white">
             <ShoppingCartIcon className="h-8 w-8" />
             <span className="text-xs font-bold">Cart</span>
           </a>
         </Link>
         <Link href="/listings">
-          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-red-700 hover:text-white">
+          <a className="lg:block lg:w-auto w-full px-3 py-2 rounded text-white hover:bg-blue-600 hover:text-white">
             <UserCircleIcon className="h-8 w-8" />
             <span className="text-xs font-bold">{session?.user?.name}</span>
           </a>
@@ -69,12 +69,12 @@ export default function Header() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header>
-        <nav className="flex flex-wrap items-center bg-red-900 p-3">
+        <nav className="flex flex-wrap items-center bg-gradient-to-r from-blue-900 via-blue-800 to-blue-700 p-3">
           <Link href="/">
             <a className="p-2 mr-4 text-white">
-              <span className="inline-flex text-xl font-bold tracking-wide">
+              <span className="inline-flex text-2xl font-bold tracking-wide">
                 {/* Placeholder icon */}
-                <FireIcon className="w-6 h-6" />
+                <FireIcon className="w-8 h-8" />
                 Skwirl
               </span>
               <span className="text-xs align-text-bottom text-transparent hover:text-white">
@@ -83,7 +83,7 @@ export default function Header() {
             </a>
           </Link>
           <button
-            className="inline-flex p-3 m-1 hover:bg-red-700 rounded lg:hidden text-white ml-auto hover:text-white"
+            className="inline-flex p-3 m-1 hover:bg-blue-600 rounded lg:hidden text-white ml-auto hover:text-white"
             onClick={handleClick}
           >
             <MenuIcon className="w-6 h-6" />
