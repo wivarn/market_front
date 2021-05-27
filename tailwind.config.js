cc58b39be2a0511e95e755a11609d026fc6a789b
Kevin Legere <kevin.legere@hey.com>
2021-05-26 21:30:24 -0700

    customized colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,33 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        success: {
+          light: "#ECFDF5",
+          DEFAULT: "#059669",
+        },
+        error: {
+          light: "#FEF2F2",
+          DEFAULT: "#DC2626",
+        },
+        primary: {
+          light: "#43B0DB",
+          DEFAULT: "#2D3142",
+          dark: "#0D3545",
+        },
+        secondary: {
+          light: "#F7A5A1",
+          DEFAULT: "#EF3E36",
+          dark: "#BD180F",
+        },
+        accent: {
+          light: "#F4F3EE",
+          DEFAULT: "#BCB8B1",
+          dark: "#2E282A",
+        }
+
+      },
   variants: {
     extend: {},
   },
