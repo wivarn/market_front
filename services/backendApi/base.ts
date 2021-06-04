b8a30e75953cc8829e6ef74312a0346b7484f7b2
wivarn <wivarn@gmail.com>
2021-06-04 16:43:37 -0700

    prefix with v0

--- services/backendApi/base.ts
+++ services/backendApi/base.ts
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
+const version = 0;
+
 export const base = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API}/v${version}`,
   timeout: 30000, // 30 seconds
   headers: {
     Accept: "application/json",
