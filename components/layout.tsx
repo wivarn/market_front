093ac8e6bd61d3e2aa9b1f175faa65f223cc7e30
wivarn <wivarn@gmail.com>
2021-06-08 18:00:59 -0700

    set max width

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="h-full">
-      <main className="mx-auto">{children}</main>
+      <main className="container mx-auto max-w-screen-2xl">{children}</main>
     </div>
   );
 }
