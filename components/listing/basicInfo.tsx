dfcaeae98bb589961f90bcaf84d535a48f85fdf4
wivarn <wivarn@gmail.com>
2021-06-10 01:49:38 -0700

    format currency

--- components/listing/basicInfo.tsx
+++ components/listing/basicInfo.tsx
@@ -5,10 +5,22 @@ const ListingBasicInfo = (props: BasicListing) => {
     <div>
       <h3 className="uppercase text-accent-darkest">{props.title}</h3>
       <div>
-        <span className="text-2xl text-accent-darker">${props.price} </span>
+        <span className="text-2xl text-accent-darker">
+          {Number(props.price).toLocaleString("en", {
+            style: "currency",
+            currency: "usd",
+          })}{" "}
+        </span>
         <span className="text-xs text-accent-dark">{props.currency}</span>
       </div>
-      <div className="text-accent-dark">+${props.domestic_shipping} Shipping</div>
+      <div className="text-accent-dark">
+        +
+        {Number(props.domestic_shipping).toLocaleString("en", {
+          style: "currency",
+          currency: "usd",
+        })}{" "}
+        Shipping
+      </div>
       <div className="font-bold uppercase text-success">{props.condition}</div>
     </div>
   );
