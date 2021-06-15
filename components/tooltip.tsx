f73723e996f157233c6c0b64506066d945c3279b
Kevin Legere <kevin.legere@hey.com>
2021-06-15 14:40:39 -0700

    minor tooltip styling change

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -5,7 +5,7 @@ interface Props {
 export function ToolTip(props:Props) {
   return (
     <div className="absolute z-10 grid items-center mt-2 -inset-4 top-10 grid-col-auto">
-      <span className="px-2 py-1 text-sm text-center rounded-full whitespace-nowrap bg-accent-darker text-accent-lightest">
+      <span className="px-1 py-1 text-sm text-center rounded-full whitespace-nowrap bg-accent-darker text-accent-lightest">
         {props.text}
       </span>
     </div>
