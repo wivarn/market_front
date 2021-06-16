cebe9884e255a2330f3595de3f93adfe0f891bd6
Kevin Legere <kevin.legere@hey.com>
2021-06-15 19:56:55 -0700

    add tooltip component

---
+++ components/tooltip.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  text: string;
+}
+
+export function ToolTip(props: Props) {
+  return (
+    <div className="absolute z-10 grid items-center mt-2 -inset-4 top-10 grid-col-auto">
+      <span className="px-1 py-1 text-sm text-center rounded-full whitespace-nowrap bg-accent-darker text-accent-lightest">
+        {props.text}
+      </span>
+    </div>
+  );
+}
