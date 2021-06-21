83cffb77efc794bee48670b463466dd7e12634e5
Kevin Legere <kevin.legere@hey.com>
2021-06-21 13:56:31 -0700

    add image blur effect

--- components/listing/preview.tsx
+++ components/listing/preview.tsx
@@ -17,6 +17,8 @@ const ListingPreview = (props: Listing): JSX.Element => {
                     alt={props.title}
                     layout="fill"
                     objectFit="cover"
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAFfSURBVHgBDY7LTuJQAEBP22uhLVpoGYQZF5MMk0kmsxhiYowf4NKFf+UP+Bm60I0uTUzUhAXEiIrv+ObValsLFa49+5NzlLX1DWlbWYpGwmc0JH7vkiQjJsmQYBRz0DjnIxwgXMemOpth8f8vnvYOeIg9TGbodJqIXJ4jUiYT1L/fJBVjDKeH/CwLaiur1BbKnHyfx7R0qkXBj2kFoYoMilCQqS3nfvNy1sWayrH8Z4R/5zNjeqgFF7VUKmLbNvghRAEOA4xCAcfQ0NJ309DRSBB23kEGHa6PG1R0lcd2m0F/SL5sIuOQyO/h+31UMaUjsianJ8+0Nptsb9VxTZv2zi5h+MHN7RXem48aBO9IoSOzUCm65OUr0+4s1YUlrDQNCnEyRnR7A9RhH3F5zJ0o8JbEdJp1otY+svaPsaKkQg4lDkL5cNHi9f4CTdNJPiMyloPXe5GB59Go1ylV5vgCEoaUs5iMbFwAAAAASUVORK5CYII="
                     className="rounded-t-md"
                   />
                 ) : null}
