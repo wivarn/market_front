b696be1cedb501bf998bc3546ff10468df71f3e5
Kevin Legere <kevin.legere@hey.com>
2021-06-21 16:56:27 -0700

    fixed header colour

--- components/forms/listing/section.tsx
+++ components/forms/listing/section.tsx
@@ -8,7 +8,7 @@ export default function FormSection({
   return (
     <section className="my-4">
       <div className="px-4 py-2 border bg-info-darker border-info-darker text-accent-lightest rounded-t-md">
-        <h4>{header}</h4>
+        <h4 className="text-accent-lightest">{header}</h4>
       </div>
       <div className="px-4 py-2 border bg-accent-lightest rounded-b-md border-info-darker">
         {children}
