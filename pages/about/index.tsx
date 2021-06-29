b310478be88ee6c60c872fb8566abc456b56a7ee
Kevin Legere <kevin.legere@hey.com>
2021-06-29 13:51:40 -0700

    remove teaser

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,12 @@
-import Teaser from "components/landing/teaser";
+import { NextSeo } from "next-seo";
 
-export default function teaser(): JSX.Element {
-  return <Teaser />;
+export default function About(): JSX.Element {
+  return (
+    <>
+      <div>
+        <NextSeo title="About us" />
+        <h3>About us</h3>
+      </div>
+    </>
+  );
 }
