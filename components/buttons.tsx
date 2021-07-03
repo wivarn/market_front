032d3eaf3f135f4faf58860d7e1d419fb0f13329
wivarn <wivarn@gmail.com>
2021-07-03 01:11:01 -0700

    refactor buttons

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,156 +1,128 @@
 import Link from "next/link";
 
 interface Props {
+  text: string;
+  type?: "button" | "submit" | "reset";
   disabled?: boolean;
   hidden?: boolean;
-  text: string;
+  href?: string;
   onClick?: () => Promise<void>;
 }
 
-interface PropsWithHref extends Props {
-  href: string;
+interface BaseButtonProps extends Props {
+  buttonClassName: string;
 }
 
-export const CTAButton = (props: PropsWithHref): JSX.Element => {
+export const CTAButton = (props: Props): JSX.Element => {
   return (
-    <Link href={props.href} passHref>
-      <button
-        type="submit"
-        className="p-2 text-2xl font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
-        disabled={props.disabled}
-        hidden={props.hidden}
-      >
-        {props.text}
-      </button>
-    </Link>
+    <_Button
+      {...props}
+      buttonClassName="p-2 text-2xl font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
+    />
   );
 };
 
-export const PrimaryButton = (props: PropsWithHref): JSX.Element => {
+export const PrimaryButton = (props: Props): JSX.Element => {
   return (
-    <Link href={props.href} passHref>
-      <button
-        type="submit"
-        className="px-2 py-1 my-1 font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
-        disabled={props.disabled}
-        hidden={props.hidden}
-      >
-        {props.text}
-      </button>
-    </Link>
+    <_Button
+      {...props}
+      buttonClassName="px-2 py-1 my-1 font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
+    />
   );
 };
 
-export const PrimaryButtonFull = (props: PropsWithHref): JSX.Element => {
+export const PrimaryButtonFull = (props: Props): JSX.Element => {
   return (
-    <Link href={props.href} passHref>
-      <button
-        type="submit"
-        className="w-full px-2 py-1 my-1 font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
-        disabled={props.disabled}
-        hidden={props.hidden}
-      >
-        {props.text}
-      </button>
-    </Link>
+    <_Button
+      {...props}
+      buttonClassName="w-full px-2 py-1 my-1 font-semibold rounded-md text-accent-lightest bg-primary disabled:bg-primary-light hover:bg-primary-dark"
+    />
   );
 };
 
-export const SecondaryButton = (props: PropsWithHref): JSX.Element => {
+export const SecondaryButton = (props: Props): JSX.Element => {
   return (
-    <Link href={props.href} passHref>
-      <button
-        type="button"
-        className="px-2 py-1 my-1 font-semibold border rounded-md border-info text-info bg-info-lightest disabled:bg-info-lighter hover:bg-info-lighter"
-        disabled={props.disabled}
-        hidden={props.hidden}
-      >
-        {props.text}
-      </button>
-    </Link>
+    <_Button
+      {...props}
+      type="button"
+      buttonClassName="px-2 py-1 my-1 font-semibold border rounded-md border-info text-info bg-info-lightest disabled:bg-info-lighter hover:bg-info-lighter"
+    />
   );
 };
 
-export const SecondaryButtonFull = (props: PropsWithHref): JSX.Element => {
+export const SecondaryButtonFull = (props: Props): JSX.Element => {
   return (
-    <Link href={props.href} passHref>
-      <button
-        type="button"
-        className="w-full px-2 py-1 my-1 font-semibold border rounded-md border-info text-info bg-info-lightest disabled:bg-info-lighter hover:bg-info-lighter"
-        disabled={props.disabled}
-        hidden={props.hidden}
-      >
-        {props.text}
-      </button>
-    </Link>
+    <_Button
+      {...props}
+      type="button"
+      buttonClassName="w-full px-2 py-1 my-1 font-semibold border rounded-md border-info text-info bg-info-lightest disabled:bg-info-lighter hover:bg-info-lighter"
+    />
   );
 };
 
 export const SubmitButton = (props: Props): JSX.Element => {
   return (
-    <button
-      type="submit"
-      className="px-2 py-1 my-1 font-semibold rounded-md bg-primary text-accent-lightest hover:bg-primary-dark disabled:bg-primary-light"
-      onClick={props.onClick}
-      disabled={props.disabled}
-      hidden={props.hidden}
-    >
-      {props.text}
-    </button>
+    <_Button
+      {...props}
+      buttonClassName="px-2 py-1 my-1 font-semibold rounded-md bg-primary text-accent-lightest hover:bg-primary-dark disabled:bg-primary-light"
+    />
   );
 };
 
 export const SecondarySubmitButton = (props: Props): JSX.Element => {
   return (
-    <button
-      type="submit"
-      className="px-2 py-1 my-1 font-semibold border rounded-md border-info bg-info-lightest text-info hover:bg-info-lighter disabled:bg-info-lighter"
-      onClick={props.onClick}
-      disabled={props.disabled}
-      hidden={props.hidden}
-    >
-      {props.text}
-    </button>
+    <_Button
+      {...props}
+      buttonClassName="px-2 py-1 my-1 font-semibold border rounded-md border-info bg-info-lightest text-info hover:bg-info-lighter disabled:bg-info-lighter"
+    />
   );
 };
 
 export const SubmitButtonFull = (props: Props): JSX.Element => {
   return (
-    <button
-      type="submit"
-      className="w-full px-2 py-2 my-1 font-semibold rounded-md bg-primary text-accent-lightest hover:bg-primary-dark disabled:bg-primary-light"
-      onClick={props.onClick}
-      disabled={props.disabled}
-      hidden={props.hidden}
-    >
-      {props.text}
-    </button>
+    <_Button
+      {...props}
+      buttonClassName="w-full px-2 py-2 my-1 font-semibold rounded-md bg-primary text-accent-lightest hover:bg-primary-dark disabled:bg-primary-light"
+    />
   );
 };
 
 export const DeleteButton = (props: Props): JSX.Element => {
   return (
+    <_Button
+      {...props}
+      buttonClassName="px-2 py-1 my-1 font-semibold border rounded-md border-error bg-error-lightest text-error hover:bg-error-lighter disabled:bg-error-lighter"
+    />
+  );
+};
+
+export const ResetButton = (props: Props): JSX.Element => {
+  return (
+    <_Button
+      {...props}
+      type="reset"
+      buttonClassName="px-2 py-1 my-1 font-semibold underline text-info disabled:text-accent"
+    />
+  );
+};
+
+const _Button = (props: BaseButtonProps): JSX.Element => {
+  const button = (
     <button
-      type="submit"
-      className="px-2 py-1 my-1 font-semibold border rounded-md border-error bg-error-lightest text-error hover:bg-error-lighter disabled:bg-error-lighter"
-      onClick={props.onClick}
+      type={props.type ? props.type : "submit"}
+      className={props.buttonClassName}
       disabled={props.disabled}
       hidden={props.hidden}
     >
       {props.text}
     </button>
   );
-};
-
-export const ResetButton = (props: Props): JSX.Element => {
-  return (
-    <button
-      type="reset"
-      className="px-2 py-1 my-1 font-semibold underline text-info disabled:text-accent"
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.text}
-    </button>
-  );
+  if (props.href) {
+    return (
+      <Link href={props.href} passHref>
+        {button}
+      </Link>
+    );
+  }
+  return button;
 };
