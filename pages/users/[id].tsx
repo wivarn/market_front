69f778c4e8cc331ee33c40bea370f624fa539851
kleverrobot <83978880+kleverrobot@users.noreply.github.com>
2021-07-26 13:05:01 -0700

    order-styles (#67)
    * split sales and purchase componenets
    
    * style tracking form
    
    * style sales and purchase pages
    
    * fixed user link bug
    
    * Updated user profile page
    
    * Update user profile page
    
    * Fixed tracking form
    
    * updated order styling again
    
    * Clean up auth forms
    
    Co-authored-by: Kevin Legere <kevin.legere@hey.com>

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -41,19 +41,24 @@ export default function ShowUser(): JSX.Element {
   const user: IUser = userResponse.data;
 
   return (
-    <div>
+    <div className="my-4">
       <PageContainer>
-        <div className="mb-4">
-          <h3 className="mb-4 text-center">Seller Profile</h3>
+        <div className="grid mb-4 text-center justify-items-center">
+          <h3 className="mb-4 text-center">User Profile</h3>
           <UserInfo givenName={user.given_name} familyName={user.family_name} />
         </div>
-        <div className="py-4">
-          <UserListingsPreview listings={user.listings} />
-        </div>
-        <div>
-          <UserReviewsPreview />
-        </div>
       </PageContainer>
+
+      <div className="my-4">
+        <PageContainer>
+          <UserListingsPreview listings={user.listings} />
+        </PageContainer>
+      </div>
+      <div className="py-4">
+        <PageContainer>
+          <UserReviewsPreview />
+        </PageContainer>
+      </div>
     </div>
   );
 }
