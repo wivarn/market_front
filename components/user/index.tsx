a68355a980d280e0345c37bc5f7a8ca01c9d5f97
Kevin Legere <kevin.legere@hey.com>
2021-08-01 22:03:04 -0700

    Add profile image to pages

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -1,17 +1,25 @@
 import { IUser } from "types/user";
-import { LgUserCircleIcon } from "components/icons";
+import Image from "next/image";
 
 export const UserInfo = (props: IUser): JSX.Element => {
   return (
-    <>
-      <LgUserCircleIcon />
+    <div className="flex items-center space-x-2">
+      <div className="container relative w-20 h-20 m-2 border rounded-full">
+        <Image
+          src={props.picture.url}
+          alt={props.given_name}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
       <span>
-        <h3>
+        <h4>
           {props.given_name} {props.family_name}
-        </h3>
-        <div className="text-sm text-accent">Location</div>
-        <div className="text-sm text-success">User Rating</div>
+        </h4>
+        <div className="text-sm text-accent-dark">Location</div>
+        <div className="text-sm text-accent-dark">User Rating</div>
       </span>
-    </>
+    </div>
   );
 };
