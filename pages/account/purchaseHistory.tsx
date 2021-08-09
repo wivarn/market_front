1686f0b9703c3dbbeeb7cbb1f5a662ca135b96bd
Kevin Legere <kevin.legere@hey.com>
2021-08-09 11:29:32 -0700

    update sales and purchases with blank state

--- pages/account/purchaseHistory.tsx
+++ pages/account/purchaseHistory.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 
+import { BlankMessage } from "components/message";
 import { GenericErrorMessage } from "components/message";
 import { IOrder } from "types/order";
+import Link from "next/link";
 import { NextSeo } from "next-seo";
 import { OrderApi } from "services/backendApi/order";
 import PageContainer from "components/pageContainer";
@@ -29,14 +31,36 @@ export default function purchaseHistory(): JSX.Element {
   if (sessionLoading || !purchases) return <SpinnerLg text="Loading..." />;
   if (error) return <GenericErrorMessage />;
 
-  return (
-    <div className="my-8">
-      <NextSeo title="Purchase History" />
-      <PageContainer yPadding="py-2">
-        <h3 className="p-2 text-center">Purchases</h3>
+  function renderPurchases() {
+    if (purchases.length == 0) {
+      return (
+        <BlankMessage>
+          <p>
+            You have not made any purchases yet.
+            <br />{" "}
+            <Link href="/">
+              <a className="underline text-info hover:text-primary">
+                Start buying now
+              </a>
+            </Link>
+          </p>
+        </BlankMessage>
+      );
+    }
+    return (
+      <>
         {purchases.map((order: IOrder) => {
           return <PurchaseOrder key={order.id} order={order} />;
         })}
+      </>
+    );
+  }
+  return (
+    <div className="my-4">
+      <NextSeo title="Purchases" />
+      <PageContainer>
+        <h3 className="pb-2 text-center">Purchases</h3>
+        {renderPurchases()}
       </PageContainer>
     </div>
   );
