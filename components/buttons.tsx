682543bdc1e8cdfb8456b7471ee1872bce68308b
Kevin Legere <kevin.legere@hey.com>
2021-08-24 12:14:11 -0700

    Go back to using default button

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -114,13 +114,11 @@ export const DeleteButton = (props: Props): JSX.Element => {
 
 export const RemoveButton = (props: Props): JSX.Element => {
   return (
-    <button
+    <_Button
       {...props}
       type="button"
-      className="p-1 border rounded-full bg-accent-lightest text-accent-dark hover:bg-accent-lighter disabled:bg-accent-lighter"
-    >
-      {props.text}
-    </button>
+      buttonClassName="px-1 border rounded-full bg-accent-lightest text-accent-dark hover:bg-accent-lighter disabled:bg-accent-lighter"
+    />
   );
 };
 
@@ -139,7 +137,7 @@ const _Button = (props: BaseButtonProps): JSX.Element => {
   const button = (
     <button
       type={props.type ? props.type : "submit"}
-      className={`px-2 py-1 my-1 text-sm sm:text-base font-semibold rounded-md disabled:cursor-not-allowed ${props.buttonClassName}`}
+      className={`${props.buttonClassName} px-2 py-1 my-1 text-sm sm:text-base font-semibold rounded-md disabled:cursor-not-allowed `}
       disabled={props.disabled || props.submitting}
       hidden={props.hidden}
       onClick={props.onClick}
