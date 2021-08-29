980e294dbd49ee6c20fe0ccc62e5c621a25e80db
Kevin Legere <kevin.legere@hey.com>
2021-08-28 19:05:00 -0700

    Updates based on Ivan's comments

--- components/googleAnalytics.tsx
+++ components/googleAnalytics.tsx
@@ -1,30 +1,11 @@
-import Script from "next/script";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-declare const window: any;
-
 export default function GoogleAnalytics(): JSX.Element {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      });
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
-      <Script
+      <script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script
+      <script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,8 +16,25 @@ import { UserSettingsProvider } from "contexts/userSettings";
 import { WixAnswers } from "components/wixAnswers";
 import { accessTokenAge } from "constants/auth";
 import { fetcher } from "services/backendApi/fetcher";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+declare const window: any;
 
 function Market({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <StrictMode>
       <Head>
