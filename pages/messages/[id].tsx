556b2a346714c7afe2edf095e35533659d856981
wivarn <wivarn@gmail.com>
2021-09-09 22:33:21 -0700

    render sidebar

--- pages/messages/[id].tsx
+++ pages/messages/[id].tsx
@@ -1,24 +1,26 @@
 import * as Yup from "yup";
 
 import { BlankMessage, GenericErrorMessage } from "components/message";
-import { SpinnerLg } from "components/spinner";
-import { useSession } from "next-auth/client";
-import { NextSeo } from "next-seo";
-import { useRouter } from "next/router";
-import useSWR, { mutate } from "swr";
 import { Form, Formik } from "formik";
-import { TextField } from "components/forms/fields";
-import { SubmitButton } from "components/buttons";
+import { IMessage, IMessageWithCorrespondents } from "types/message";
+import { Spinner, SpinnerLg } from "components/spinner";
+import useSWR, { mutate } from "swr";
+
+import Image from "next/image";
+import Link from "next/link";
 import { MessageApi } from "services/backendApi/message";
-import { toast } from "react-toastify";
+import { NextSeo } from "next-seo";
 import PageContainer from "components/pageContainer";
-import { IMessage } from "types/message";
-import { IUser } from "types/user";
+import { SubmitButton } from "components/buttons";
+import { TextField } from "components/forms/fields";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/client";
 
 export default function SendMessage(): JSX.Element {
   const router = useRouter();
   const { id } = router.query;
-  const [session, sessionLoading] = useSession();
+  const [session] = useSession();
 
   function getMessages() {
     const { data, error } = useSWR(
@@ -32,15 +34,84 @@ export default function SendMessage(): JSX.Element {
     };
   }
 
+  function getLatestMessages() {
+    const { data, error } = useSWR(
+      session ? [`messages`, session.accessToken] : null
+    );
+
+    return {
+      latestMessagesResponse: data,
+      latestMessagesLoading: !error && !data,
+      latestMessagesError: error,
+    };
+  }
+
   const { messagesResponse, messagesLoading, messagesError } = getMessages();
 
-  if (sessionLoading || messagesLoading) return <SpinnerLg text="Loading..." />;
-  if (messagesError) return <GenericErrorMessage />;
+  function renderLatestMessages() {
+    const {
+      latestMessagesResponse,
+      latestMessagesLoading,
+      latestMessagesError,
+    } = getLatestMessages();
 
-  const correspondent: IUser = messagesResponse.data.correspondent;
-  const messages: IMessage[] = messagesResponse.data.messages;
+    if (latestMessagesLoading) return <Spinner text="Loading..." />;
+    if (latestMessagesError)
+      return <span>Unable to load latest messages.</span>;
+
+    const latestMessages: IMessageWithCorrespondents[] =
+      latestMessagesResponse.data;
+
+    return (
+      <div>
+        {latestMessages.map((message) => {
+          return renderLatestMessage(message);
+        })}
+      </div>
+    );
+
+    function renderLatestMessage(message: IMessageWithCorrespondents) {
+      const otherUser =
+        message.sender_id == session?.accountId
+          ? message.recipient
+          : message.sender;
+      const currentChat = id && id == otherUser.id;
+      return (
+        <Link href={`/messages/${otherUser.id}`}>
+          <a
+            key={message.created_at}
+            className={`flex items-center rounded h-20 ${
+              currentChat ? "bg-info-lighter" : ""
+            }`}
+          >
+            <div className="container relative w-16 h-16 m-2 border rounded-full">
+              <Image
+                src={otherUser.picture.url || "/ProfilePlaceholder.svg"}
+                alt={otherUser.full_name}
+                width="64"
+                height="64"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+            <span className="">
+              <div className="font-bold">{otherUser.full_name}</div>
+              <div className="w-56 text-sm truncate">{message.body}</div>
+              <div className="text-xs text-accent-dark">
+                {message.created_at}
+              </div>
+            </span>
+          </a>
+        </Link>
+      );
+    }
+  }
 
   function renderMessages() {
+    if (messagesLoading) return <SpinnerLg text="Loading..." />;
+    if (messagesError) return <GenericErrorMessage />;
+
+    const messages: IMessage[] = messagesResponse.data.messages;
     if (!messages.length) return renderNoMessages();
 
     return (
@@ -62,12 +133,13 @@ export default function SendMessage(): JSX.Element {
     const currentUserMessage = message.sender_id == session?.accountId;
     return (
       <div
+        key={message.created_at}
         className={`w-1/2 ${
           currentUserMessage ? "justify-self-end" : "justify-self-start"
         }`}
       >
         <div
-          className={`p-1 m-1 mx-auto border rounded ${
+          className={`p-1 m-1 mx-auto rounded ${
             currentUserMessage ? "bg-info-lighter" : "bg-accent-lighter"
           }`}
         >
@@ -79,6 +151,8 @@ export default function SendMessage(): JSX.Element {
   }
 
   function renderMessageForm() {
+    if (messagesLoading || messagesError) return null;
+
     const messageSchema = Yup.object().shape({
       body: Yup.string()
         .min(1, "Must be at least one character")
@@ -129,8 +203,13 @@ export default function SendMessage(): JSX.Element {
     <>
       <NextSeo title="Message History" />
       <PageContainer>
-        {renderMessages()}
-        {renderMessageForm()}
+        <div className="flex space-x-2">
+          <div className="flex-none border w-80">{renderLatestMessages()}</div>
+          <div className="flex-initial w-full h-screen overflow-y-auto border">
+            {renderMessages()}
+            {renderMessageForm()}
+          </div>
+        </div>
       </PageContainer>
     </>
   );
--- types/message.ts
+++ types/message.ts
@@ -1,6 +1,12 @@
+import { IUser } from "types/user";
 export interface IMessage {
   sender_id: string;
   recipient_id: string;
   body: string;
   created_at: string;
+}
+
+export interface IMessageWithCorrespondents extends IMessage {
+  sender: IUser;
+  recipient: IUser;
 }
