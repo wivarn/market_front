31e939ea695735d4671a2ab46de2442c021844c6
wivarn <wivarn@gmail.com>
2021-09-26 16:30:39 -0700

    unused code

--- components/iconButton.tsx
+++ components/iconButton.tsx
@@ -2,29 +2,11 @@ import React, { ReactNode } from "react";
 
 import Link from "next/link";
 
-interface ButtonProps {
-  icon: ReactNode;
-  disabled?: boolean;
-}
-
 interface LinkProps {
   icon: ReactNode;
   url: string;
   target?: string;
 }
-
-export const IconButton = (props: ButtonProps): JSX.Element => {
-  return (
-    <div className="group">
-      <button
-        className="relative items-center p-1 border rounded-md disabled:cursor-not-allowed disabled:opacity-50 border-accent group-hover:border-primary text-accent-darker group-hover:text-primary"
-        disabled={props.disabled}
-      >
-        {props.icon}
-      </button>
-    </div>
-  );
-};
 
 export const IconButtonLink = (props: LinkProps): JSX.Element => {
   return (
