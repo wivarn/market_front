5ae969892c485f887d97667633a6094bb468290f
wivarn <wivarn@gmail.com>
2021-11-14 17:52:32 -0800

    add spinner when settings are not loaded

--- pages/offers.tsx
+++ pages/offers.tsx
@@ -1,19 +1,18 @@
+import { useContext, useEffect } from "react";
+
+import { GenericErrorMessage } from "components/message";
 import { NextSeo } from "next-seo";
 import PageContainer from "components/pageContainer";
 import PurchaseOffers from "components/offer/purchaseOffers";
 import SalesOffers from "components/offer/salesOffers";
+import { SpinnerLg } from "components/spinner";
+import { UserSettingsContext } from "contexts/userSettings";
 import useSWR from "swr";
 import { useSession } from "next-auth/client";
-import { UserSettingsContext } from "contexts/userSettings";
-import { useContext, useEffect } from "react";
-import { SpinnerLg } from "components/spinner";
-import { GenericErrorMessage } from "components/message";
 
 export default function Offers(): JSX.Element {
   const [session, sessionLoading] = useSession();
   const { userSettings, assignUserSettings } = useContext(UserSettingsContext);
-  const purchaseOffers = userSettings.offers.purchase_offers;
-  const saleOffers = userSettings.offers.sale_offers;
 
   const getOffers = () => {
     const { data, error } = useSWR(
@@ -35,15 +34,16 @@ export default function Offers(): JSX.Element {
     }
   }, [offersResponse]);
 
-  if (sessionLoading || offersLoading) return <SpinnerLg text="Loading..." />;
+  if (sessionLoading || userSettings.default_settings || offersLoading)
+    return <SpinnerLg text="Loading..." />;
   if (offersError) return <GenericErrorMessage />;
 
   return (
     <div className="my-4">
       <NextSeo title="Purchases" />
       <PageContainer>
-        <PurchaseOffers offers={purchaseOffers} />
-        <SalesOffers offers={saleOffers} />
+        <PurchaseOffers offers={userSettings.offers.purchase_offers} />
+        <SalesOffers offers={userSettings.offers.sale_offers} />
       </PageContainer>
     </div>
   );
