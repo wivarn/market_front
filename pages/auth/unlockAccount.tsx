7464b5e9c91e0332bb3cc0741e08e2f533d35054
wivarn <wivarn@gmail.com>
2021-12-01 03:49:05 -0800

    error message on some auth pages

--- pages/auth/unlockAccount.tsx
+++ pages/auth/unlockAccount.tsx
@@ -1,11 +1,16 @@
+import { useEffect, useState } from "react";
+
 import { AuthApi } from "services/backendApi/auth";
+import { GenericErrorMessage } from "components/message";
 import { NextSeo } from "next-seo";
+import PageContainer from "components/pageContainer";
+import { SpinnerLg } from "components/spinner";
 import { toast } from "react-toastify";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function unlockAccount(): JSX.Element {
   const router = useRouter();
+  const [errorPage, setErrorPage] = useState(false);
 
   useEffect(() => {
     if (!router.isReady) return;
@@ -20,13 +25,22 @@ export default function unlockAccount(): JSX.Element {
           router.push("/");
         })
         .catch((error) => {
+          setErrorPage(true);
           toast.error(error.response.data.error);
         });
     }
   }, [router.isReady]);
+
+  if (errorPage)
+    return (
+      <PageContainer>
+        <GenericErrorMessage />
+      </PageContainer>
+    );
   return (
     <>
       <NextSeo title="Unlock Account" />
+      <SpinnerLg text="Loading..." />
     </>
   );
 }
--- pages/auth/verifyAccount.tsx
+++ pages/auth/verifyAccount.tsx
@@ -1,13 +1,17 @@
+import { useEffect, useState } from "react";
+
 import { AuthApi } from "services/backendApi/auth";
+import { GenericErrorMessage } from "components/message";
 import { NextSeo } from "next-seo";
+import PageContainer from "components/pageContainer";
 import { SpinnerLg } from "components/spinner";
 import { signIn } from "next-auth/react";
 import { toast } from "react-toastify";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function verifyAccount(): JSX.Element {
   const router = useRouter();
+  const [errorPage, setErrorPage] = useState(false);
 
   useEffect(() => {
     if (!router.isReady) return;
@@ -26,10 +30,18 @@ export default function verifyAccount(): JSX.Element {
           router.push("/account/address");
         })
         .catch((error) => {
+          setErrorPage(true);
           toast.error(error.response.data.error);
         });
     }
   }, [router.isReady]);
+
+  if (errorPage)
+    return (
+      <PageContainer>
+        <GenericErrorMessage />
+      </PageContainer>
+    );
   return (
     <>
       <NextSeo title="Verify Account" />
