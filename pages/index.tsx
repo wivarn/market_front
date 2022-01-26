ec908644997eac724e63edd91ba6614da6888a6a
Kevin Legere <kevin@skwirl.io>
2022-01-26 13:39:21 -0800

    try webp

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home(): JSX.Element {
               <PrimaryButton text="Sign up now" href="/account/new" />
             </div>
             <img
-              src="/assets/hero-cards.png"
+              src="/assets/hero-cards.webp"
               height={638}
               width={600}
               alt="Card Hero"
