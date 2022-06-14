45dee2f104eae65635d4329176df9c4bbc4fef89
wivarn <wivarn@gmail.com>
2022-06-13 19:11:49 -0700

    sad banner

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,5 @@
 import Header from "./header";
+import Link from "next/link";
 import dynamic from "next/dynamic";
 
 const Footer = dynamic(() => import("./footer"));
@@ -11,6 +12,14 @@ export default function Layout({
   return (
     <div className="flex flex-col h-screen">
       <Header />
+      <div className="w-full bg-warning">
+        <p className="py-2 text-xl text-center text-info">
+          This website is for demo purposes only and is no longer operational.{" "}
+          <Link href="/about">
+            <a className="text-white underline">Learn more.</a>
+          </Link>
+        </p>
+      </div>
       <main className="flex-grow bg-accent-lightest">{children}</main>
       <Footer />
     </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { NextSeo } from "next-seo";
 import PageContainer from "components/pageContainer";
 
@@ -8,58 +7,18 @@ export default function About(): JSX.Element {
       <div>
         <NextSeo title="About us" />
         <PageContainer yPadding="py-8 my-8 h-full">
-          <h1 className="text-center h3">About us</h1>
-          <div className="grid max-w-2xl grid-cols-1 py-8 mx-auto mt-4 space-y-8 md:grid-cols-2 justify-items-center md:space-y-0">
-            <div className="grid grid-cols-1 text-center justify-items-center">
-              <div className="mb-4">
-                <Image
-                  src="/kevin-profile.webp"
-                  height={200}
-                  width={200}
-                  alt="kevin"
-                />
-              </div>
-              <h2 className="h4">Kevin Legere</h2>
-              <p className="text-accent-dark">CEO | Founder</p>
-            </div>
-            <div className="grid grid-cols-1 text-center justify-items-center">
-              <div className="mb-4">
-                <Image
-                  src="/ivan-profile.webp"
-                  height={200}
-                  width={200}
-                  alt="ivan"
-                />
-              </div>
-              <h2 className="h4">Ivan Wong</h2>
-              <p className="text-accent-dark">CTO | Co-Founder</p>
-            </div>
-          </div>
-          <div className="max-w-xl mx-auto my-8">
-            <h2 className="pb-4">Who are we?</h2>
-            <p>
-              We are a couple of friends who live in beautiful British Columbia,
-              Canada and have a vision for providing a unique online experience
-              for collectors.
-            </p>
-            <h2 className="py-4 h4">What is Skwirl?</h2>
-            <p>
-              Skwirl is a social marketplace where collectors can buy, sell and
-              admire sports cards, trading cards and collectibles.
-            </p>
-            <h2 className="py-4 h4">Have a question?</h2>
-            <p>
-              We would love to hear from you! You can contact us{" "}
-              <a
-                href="https://support.skwirl.io/kb/en/contact"
-                target="_blank"
-                rel="noreferrer"
-                className="underline text-info hover:text-primary"
-              >
-                here
-              </a>
-              .
-            </p>
+          <h1 className="pb-3 text-center h3">We&apos;re closing up shop.</h1>
+          <div className="whitespace-pre-line ">
+            It&apos;s been a great ride connecting with so many collectors over
+            the last year. Unfortunately, we have decided to move on to some
+            other projects and Skwirl will no longer be supported or maintained.
+            We have disabled the ability to purchase items from the site. If you
+            are interested in forking the code or purchasing Skwirl you can
+            contact us.
+            {"\n\n"}
+            Thanks so much for your support!
+            {"\n\n"}
+            Skwirl Team.
           </div>
         </PageContainer>
       </div>
